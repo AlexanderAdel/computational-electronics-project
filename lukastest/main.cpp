94242d95bd08d145c2bae3016e676540853074c8
lukasheisler-git <lukas.heisler@yahoo.de>
2021-12-10 12:02:55 +0100

    Added Cmake and main for Poisson

--- lukastest/main.cpp
+++ lukastest/main.cpp
@@ -1,57 +1,10 @@
-#include <deal.II/grid/tria.h>
-#include <deal.II/grid/tria_accessor.h>
-#include <deal.II/grid/tria_iterator.h>
-#include <deal.II/grid/grid_generator.h>
-#include <deal.II/grid/grid_out.h>
+#include "poisson.hpp"
 #include <iostream>
 #include <fstream>
 #include <cmath>
 
-
-
-using namespace dealii;
-void first_grid()
-{
-  Triangulation<2> triangulation;
-  GridGenerator::hyper_cube(triangulation);
-  triangulation.refine_global(4);
-  std::ofstream out("grid-1.vtk");
-  GridOut       grid_out;
-  grid_out.write_vtk(triangulation, out);
-  std::cout << "Grid written to grid-1.vtk" << std::endl;
-}
-void second_grid()
-{
-  Triangulation<2> triangulation;
-  const Point<2> center(1, 0);
-  const double   inner_radius = 0.5, outer_radius = 1.0;
-  GridGenerator::hyper_shell(
-    triangulation, center, inner_radius, outer_radius, 10);
-  for (unsigned int step = 0; step < 5; ++step)
-    {
-      for (auto cell : triangulation.active_cell_iterators())
-        {
-          for (unsigned int v = 0; v < GeometryInfo<2>::vertices_per_cell; ++v)
-            {
-              const double distance_from_center =
-                center.distance(cell->vertex(v));
-              if (std::fabs(distance_from_center - inner_radius) < 1e-10)
-                {
-                  cell->set_refine_flag();
-                  break;
-                }
-            }
-        }
-      triangulation.execute_coarsening_and_refinement();
-    }
-  std::ofstream out("grid-2.vtk");
-  GridOut       grid_out;
-  grid_out.write_vtk(triangulation, out);
-  std::cout << "Grid written to grid-2.vtk" << std::endl;
-  triangulation.reset_manifold(0);
-}
-int main()
-{
-  first_grid();
-  second_grid();
+int main(){
+  Poisson poisson_problem;
+  poisson_problem.prepare();
+  poisson_problem.run();
 }
